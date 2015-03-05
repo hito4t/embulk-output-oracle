eb9cc7b01091ed68a998952d0b01fdb77fb5af1c
hito4t <thitoshi@cac.co.jp>
2015-03-05 18:58:05 +0900

    テストクラスを作成

---
+++ src/test/java/org/embulk/output/oracle/OracleTest.java
@@ -0,0 +1,14 @@
+/*
+ * $Id: typical.epf 2627 2010-03-18 01:40:13Z tiba $
+ */
+package org.embulk.output.oracle;
+
+import org.embulk.cli.Main;
+
+public class OracleTest {
+	
+	public static void main(String[] args) {
+		Main.main(new String[]{"run", "src/test/resources/test1.yml"});
+	}
+	
+}
