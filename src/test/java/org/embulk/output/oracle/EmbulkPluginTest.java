afbb34a9456949a4caddb069c5f2205fa32f653f
hito4t <thitoshi@cac.co.jp>
2015-03-06 18:20:57 +0900

    リファクタリング

---
+++ src/test/java/org/embulk/output/oracle/EmbulkPluginTest.java
@@ -0,0 +1,14 @@
+/*
+ * $Id: typical.epf 2627 2010-03-18 01:40:13Z tiba $
+ */
+package org.embulk.output.oracle;
+
+import org.embulk.cli.Main;
+
+public class EmbulkPluginTest {
+	
+	protected static void execute(String... args) {
+		Main.main(args);
+	}
+	
+}
